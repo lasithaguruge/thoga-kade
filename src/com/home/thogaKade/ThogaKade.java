7e28e270a859ea8adef70a138f577c6a44f65280
lasithaguruge <lasithaguruge91@gmail.com>
2018-04-17 23:41:43 +0530

    Test ssh key

--- src/com/home/thogaKade/ThogaKade.java
+++ src/com/home/thogaKade/ThogaKade.java
@@ -12,5 +12,6 @@ package com.home.thogaKade;
 public class ThogaKade {
     public static void main(String[] args) {
         System.out.println("Welcome to thoga kade");
+        System.out.println("Test with SSH Key");
     }
 }
